6153b812e7633f4a53e19fc2802a306175735d84
yehonatanyosefi <123459744+yehonatanyosefi@users.noreply.github.com>
2023-06-16 13:14:03 +0300

    Refactor sidebar to include a collapsible menu

--- src/cmps/MainWrapper.jsx
+++ src/cmps/MainWrapper.jsx
@@ -1,10 +1,9 @@
 import Sidebar from './Sidebar'
 export default function MainWrapper({ children }) {
-	//TODO fix progress bar
 	return (
-		<div className="grid grid-cols-[244px_minmax(500px,_1fr)] mx-auto overflow-hidden">
+		<div className="grid md:grid-cols-[244px_minmax(500px,_1fr)] grid-cols-1 mx-auto overflow-hidden">
 			<Sidebar />
-			<div className="h-screen overflow-auto min-w-full ml-10">{children}</div>
+			<div className="h-screen overflow-auto min-w-full md:ml-10">{children}</div>
 		</div>
 	)
 }
--- src/cmps/Sidebar.jsx
+++ src/cmps/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { HOME, CREATE, PIC_CLASH } from '../routes'
 import SvgIcon from './util/SvgIcon'
@@ -17,29 +18,41 @@ export default function Sidebar() {
 		{ to: CREATE, text: 'Create', svg: 'plus' },
 		{ to: `/p/${username || 'Guest'}`, text: 'Profile', svg: null, profile: true },
 	]
+
+	const [isOpen, setIsOpen] = useState(false)
+
 	return (
-		<div className="mr-10 pt-0.5 pb-0.5 pl-1 pr-1 h-screen flex flex-col gap-4 items-start select-none border-r border-gray-300 bg-white w-[244px]">
-			<Link to={HOME}>
-				<img className="m-3 m-w-full" src={`${process.env.PUBLIC_URL}/img/logo.png`} />
-			</Link>
-			{links.map(({ to, text, svg, profile }) => (
-				<Link
-					key={to}
-					to={to}
-					className="flex items-center justify-start py-[10px] px-4 ml-2 mr-4 w-[220px] rounded-3xl hover:bg-gray-300 transition-all duration-250 cursor-pointer transition-all duration-250 link"
-					aria-label={text}>
-					<div className="flex-shrink-0 w-[24px] h-[24px]">
-						{svg && <SvgIcon className="w-full h-full" iconName={svg} />}
-						{profile && (
-							<img
-								className="w-full h-full rounded-full"
-								src={avatarUrl || `${process.env.PUBLIC_URL}/img/avatars/default.png`}
-							/>
-						)}
-					</div>
-					<span className="ml-4 text-sm">{text}</span>
+		<div>
+			<button onClick={() => setIsOpen(!isOpen)} className="md:hidden block">
+				<SvgIcon className="w-6 h-6" iconName="menu" />
+			</button>
+
+			<div
+				className={`${
+					isOpen ? 'block' : 'hidden'
+				} md:block md:w-[244px] md:h-screen md:border-r md:mr-10 md:pt-0.5 md:pb-0.5 md:pl-1 md:pr-1 md:flex md:flex-col md:gap-4 md:items-start md:border-gray-300 md:bg-white`}>
+				<Link to={HOME}>
+					<img className="m-3 m-w-full" src={`${process.env.PUBLIC_URL}/img/logo.png`} />
 				</Link>
-			))}
+				{links.map(({ to, text, svg, profile }) => (
+					<Link
+						key={to}
+						to={to}
+						className="flex items-center justify-start py-[10px] px-4 ml-2 mr-4 w-[220px] rounded-3xl hover:bg-gray-300 transition-all duration-250 cursor-pointer transition-all duration-250 link"
+						aria-label={text}>
+						<div className="flex-shrink-0 w-[24px] h-[24px]">
+							{svg && <SvgIcon className="w-full h-full" iconName={svg} />}
+							{profile && (
+								<img
+									className="w-full h-full rounded-full"
+									src={avatarUrl || `${process.env.PUBLIC_URL}/img/avatars/default.png`}
+								/>
+							)}
+						</div>
+						<span className="ml-4 text-sm">{text}</span>
+					</Link>
+				))}
+			</div>
 		</div>
 	)
 }
