d3ade4149adcb268b206a7f9be8c8c62af7fff74
yehonatanyosefi <123459744+yehonatanyosefi@users.noreply.github.com>
2023-06-16 13:21:29 +0300

    Adjust MainWrapper width for better display

--- src/cmps/MainWrapper.jsx
+++ src/cmps/MainWrapper.jsx
@@ -3,7 +3,7 @@ export default function MainWrapper({ children }) {
 	return (
 		<div className="grid md:grid-cols-[244px_minmax(500px,_1fr)] grid-cols-1 mx-auto overflow-hidden">
 			<Sidebar />
-			<div className="h-screen overflow-auto min-w-full md:ml-10">{children}</div>
+			<div className="h-screen overflow-auto min-w-[85%] max-w-full md:ml-10">{children}</div>
 		</div>
 	)
 }
